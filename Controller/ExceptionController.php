df3a1d75e98d2d2c50f8cb7a6cb48873ef9d4fe5
bltnico <bellot.n@gmail.com>
2014-12-13 22:54:20 +0100

    Add ExceptionController

---
+++ Controller/ExceptionController.php
@@ -0,0 +1,48 @@
+<?php 
+
+namespace Bltnico\Bundle\FormPolymerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Polymer input Exception
+ *
+ * @name ExceptionController
+ * @author bltnico
+ * @version 0.1
+ */
+class ExceptionController extends Controller 
+{
+	/**
+	 *
+	 * @throw Exception
+	 */
+	public function formNotExistAction()
+	{
+		throw new \Exception("The controller must return a form"); 
+	}
+
+	/**
+	 * 
+	 * @param (string) $field
+	 * @throw Exception
+	 */
+	public function ignoreFieldNotExistAction($field) 
+	{
+		$err = sprintf("Field %s doesn't exist.", $field);
+
+		throw new \Exception($err);
+	}
+
+	/**
+	 * 
+	 * @param (string) $type
+	 * @throw Exception
+	 */
+	public function unknownFieldTypeAction($type) 
+	{
+		$err = sprintf("Unknow field %s", $type);
+
+		throw new \Exception($err);
+	}
+}
