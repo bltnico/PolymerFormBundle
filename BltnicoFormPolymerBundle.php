4b71ef4c299eafd7e3ab08fb3b5e97294ea0674f
bltnico <bellot.n@gmail.com>
2014-12-13 22:47:21 +0100

    Create bundle file

---
+++ BltnicoFormPolymerBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 
+ * @author:  bltnico <contact@nicolasbellot.fr>
+ *
+ */
+
+namespace Bltnico\Bundle\FormPolymerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BltnicoFormPolymerBundle extends Bundle
+{
+}
